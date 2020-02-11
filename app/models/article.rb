6ffc79decc224f6d7cb3d1d2e20e6fda770d0773
Rory Hellier <rory.hellier@googlemail.com>
2020-02-10 19:00:49 -0500

    Added extra security for blog app. Deletion of tags, articles and authors removed unless signed in. Creation of new authors removed unless via invite. Visual deletion buttons removed from views index pages.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,7 @@
 class Article < ApplicationRecord
   has_many :comments
   has_many :taggings
-  has_many :tags, through: :taggings, dependent: :destroy
+  has_many :tags, through: :taggings
   has_attached_file :image, styles: {large: "1280x720>", medium: "800x450>", small: "400x225>", thumb: "100x100>"}
   validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/png', 'image/gif']
 
